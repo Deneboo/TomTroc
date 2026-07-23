cd3550ce0b193b0bed67b5bc228cb9cffe740236
Deneboo <deneborbit@hotmail.com>
2026-07-23 18:38:03 +0200

    add register and login page

--- src/Views/Templates/Site/register.php
+++ src/Views/Templates/Site/register.php
@@ -1,1 +1,23 @@
-<h1>Inscription</h1>
+<section class="page-container">
+    <section class="register-login-page">
+  <div class="form-section">
+    <h1>Inscription</h1>
+    <form class="register-login-form">
+        <div>
+            <label for="Pseudo">Pseudo</label>
+            <input type="text" name="username" id="username" required>
+            </div>
+             <div>
+            <label for="Email">Email</label>
+            <input type="text" name="email" id="email" required>
+            </div>
+            <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" required>
+            </div>
+            <button class="submit btn btn-primary">Se connecter</button>
+        </form>
+  </div>
+  <img src="/assets/images/library-vintage.jpg" alt="image de livres">
+    </section>
+</section>
